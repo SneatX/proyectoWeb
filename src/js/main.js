d95eee3bef26ede3a0194e1218f34c00f8f3c0d3
SneatX <santi02005@hotmail.com>
2024-05-21 21:37:17 -0500

    feat: :sparkles: Nav bar para mobil funcional

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,25 @@
-import { desplegarBarra } from "./modules/navBar.js";
-import { limpiarInterfaz} from "./modules/ui/limpiarInterfaz.js"
-import { filtroElegido } from "./modules/ui/filtroElegido.js";
+import { filtroElegidoPc, filtroElegidoMobil} from "./modules/ui/filtroElegido.js";
 import { calcularCantidadCarrito } from "./modules/ui/cantidadCarrito.js";
-
-/*Barra desplegable*/
-const btnDesplegar = document.getElementById('btnDesplegarMenu');
-btnDesplegar.addEventListener('click', desplegarBarra);
 
 /*Inicio en todos los productos*/ 
 document.addEventListener("DOMContentLoaded" , e =>{
     e.preventDefault()
-    filtroElegido("mostrarTodosPc")
+    filtroElegidoPc("mostrarTodosPc")
     calcularCantidadCarrito()
 })
 
 /*Botones de filtro en pc*/
 const botonesNavPc = document.querySelector(".nav__pc")
 botonesNavPc.addEventListener("click" , e=>{
-    filtroElegido(e.target.id) //se pasa el id del boton por parametro
+    let posiblesCasosPc = ["mostrarTodosPc", "mostrarAbrigosPc", "mostrarCamisasPc", "mostrarPantalonesPc", "btnCarrito"]
+    if(posiblesCasosPc.includes(e.target.id)) filtroElegidoPc(e.target.id)
 })
 
-
-const btnCarritoMovil = document.getElementById("btnCarritoMov")
-btnCarritoMovil.addEventListener("click" , e=>{
-    e.preventDefault()
-    limpiarInterfaz()
-    filtroElegido("btnCarrito")
-
-    /*Estilos de los botones al mostrar*/
-    btnCarritoMovil.classList.add("paginaElegida")
-    document.querySelector(".container__main").style.display ="none"
-    document.querySelector(".container__mainCarrito").style.display ="block"
+/*Botones de filtro en mobil*/
+const botonesNavMov = document.querySelector(".nav__movile")
+botonesNavMov.addEventListener("click" , e=>{
+    let posiblesCasosMobil = ["btnDesplegarMenu", "mostrarTodosMob", "mostrarAbrigosMob", "mostrarCamisasMob", "mostrarPantalonesMob", "btnCarritoMob"]
+    if(posiblesCasosMobil.includes(e.target.id)) filtroElegidoMobil(e.target.id)
 })
+
 
--- src/js/modules/ui/filtroElegido.js
+++ src/js/modules/ui/filtroElegido.js
@@ -1,5 +1,7 @@
 import { mostrarTodos, mostrarAbrigos, mostrarCamisetas, mostrarPantalones, mostrarCarrito} from "../filtrarElementosPc"
 import { limpiarInterfaz } from "./limpiarInterfaz"
+import { desplegarBarra } from "./navBar"
+
 const btnMostrarTodosPc = document.getElementById("mostrarTodosPc")
 const btnMostrarAbrigosPc = document.getElementById("mostrarAbrigosPc")
 const btnMostrarCamisasPc = document.getElementById("mostrarCamisasPc")
@@ -7,7 +9,7 @@ const btnMostrarPantalonesPc = document.getElementById("mostrarPantalonesPc")
 const btnCarritoPc = document.getElementById("btnCarrito")
 
 
-export const filtroElegido = (cod)=>{
+export const filtroElegidoPc = (cod)=>{
     document.querySelector(".container__main").style.display ="block"
     document.querySelector(".container__mainCarrito").style.display ="none"
     limpiarInterfaz()
@@ -36,6 +38,46 @@ export const filtroElegido = (cod)=>{
             mostrarCarrito()
             btnCarritoPc.classList.add("paginaElegida")
             document.querySelector(".main__carrito").innerHTML = ""
+            document.querySelector(".container__main").style.display ="none"
+            document.querySelector(".container__mainCarrito").style.display ="block"
+    }
+}
+
+export const filtroElegidoMobil = (cod) =>{
+
+
+    if(cod != "btnDesplegarMenu"){
+        limpiarInterfaz()
+        document.querySelector(".container__main").style.display ="block"
+        document.querySelector(".container__mainCarrito").style.display ="none"
+    }
+        
+    switch(cod){
+        case "btnDesplegarMenu":
+            desplegarBarra()
+            break
+        case "mostrarTodosMob":
+            mostrarTodos()
+            btnMostrarTodosPc.classList.add("paginaElegida")
+            document.getElementById("titleContainers").textContent = "Todos los productos"
+            break
+        case "mostrarAbrigosMob":
+            mostrarAbrigos()
+            btnMostrarAbrigosPc.classList.add("paginaElegida")
+            document.getElementById("titleContainers").textContent = "Abrigos"
+            break
+        case "mostrarCamisasMob":
+            mostrarCamisetas()
+            btnMostrarCamisasPc.classList.add("paginaElegida")    
+            document.getElementById("titleContainers").textContent = "Camisetas"
+            break
+        case "mostrarPantalonesMob":
+            mostrarPantalones()
+            btnMostrarPantalonesPc.classList.add("paginaElegida")
+            document.getElementById("titleContainers").textContent = "Pantalones"
+            break
+        case "btnCarritoMob":
+            mostrarCarrito()
             document.querySelector(".container__main").style.display ="none"
             document.querySelector(".container__mainCarrito").style.display ="block"
     }
